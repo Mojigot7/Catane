9c21bb0599d0043c08f6b031199991cd85650e7a
Zhou Alexandre <zhoualexandre@gmail.com>
2021-12-29 21:19:27 +0100

    Carte Dev fini

--- projetYA/src/main/java/inventaire/Developpement.java
+++ projetYA/src/main/java/inventaire/Developpement.java
@@ -1,87 +1,70 @@
-/*package inventaire;
+package inventaire;
 
 import java.util.LinkedList;
 
 public class Developpement {
-    LinkedList stock = new LinkedList<Developpement>();
 
-    public Developpement(){
-        int aleatoire;
-        aleatoire = (int)(Math.random()*2);
+    private String nom;
+    private LinkedList<Developpement> stock;
+
+    public Developpement(LinkedList<Developpement> stock){
+        this.stock = stock;
+    }
+
+    public Developpement(String nom,LinkedList<Developpement> stock){
+        this.nom = nom;
+        this.stock = stock;
+    }
+
+    public void creationCarteDev(LinkedList<Developpement>stock){
+        int aleatoire = (int)(Math.random()*2);
         if(aleatoire == 0){
-            PdVictoire tmp = new PdVictoire();
-            this.add(tmp);
+            Developpement tmp = new Developpement("Point de Victoire",stock);
+            stock.add(tmp);
         }
         if(aleatoire == 1){
-            int aleatoirerprogres;
-            aleatoirerprogres = (int)(Math.random()*2);
-            addprogres(aleatoirerprogres);
+            Developpement tmp = new Developpement("Chevalier",stock);
+            stock.add(tmp);
         }
         if(aleatoire == 2){
-            Chevalier tmp = new Chevalier();
-            this.add(tmp);
+            int aleaprogres = (int)(Math.random()*2);
+            if(aleaprogres == 0){
+                Developpement tmp = new Developpement("Construction de route",stock);
+                stock.add(tmp);
+            }
+            if(aleaprogres == 1){
+                Developpement tmp = new Developpement("Invention",stock);
+                stock.add(tmp);
+            }
+            if(aleaprogres == 2){
+                Developpement tmp = new Developpement("Monopole",stock);
+                stock.add(tmp);
+            }
         }
         else{
             System.out.println("BUG Developpement constructor !");
         }
     }
 
-    public void addprogres(int a){
-        if(a == 0){
-            Construction tmp = new Construction();
-            this.add(tmp);
-        }
-        if(a == 1){
-            Invention tmp = new Invention();
-            this.add(tmp);
-        }
-        if(a == 2){
-            Monopole tmp = new Monopole();
-            this.add(tmp);
-        }
-        else{
-            System.out.println("Bug on addprogres method !");
-        }
+    public String getNom(){
+        return this.nom;
     }
 
-    public void add(Object e){
-        if(e instanceof PdVictoire || e instanceof Construction || e instanceof Chevalier || e instanceof Invention || e instanceof Monopole){
-            stock.add(e);
-        }
-        else{
-            System.out.println("BUG on add(In Developpement class) !");
-        }
+    public LinkedList<Developpement> getCarteDev(){
+        return this.stock;
     }
 
-    public class PdVictoire {
-        
-        private int points = 1;
-        
-        public PdVictoire(){}
-
-        public int getPoints(){
-            return points;
+    public void afficheCarteDev(){
+        for(int i = 0 ; i < stock.size() ; i++){
+            System.out.println(stock.get(i).nom);
         }
     }
-
-    public class Construction{
-        //private int quantite; // Il doit y avoir maximun 2 carte de construction
-
-    }
-
-    public class Invention{
-        //private int quantite; // Max 2 carte Invention
-    }
-
-    public class Monopole{
-        //private int quantite; // Max 2 carte Monpole
-    }
-
-
-    public class Chevalier{
-
-        public Chevalier(){}
-
+    public static void main(String [] agrs){
+        LinkedList<Developpement> test = new LinkedList<>();
+        Developpement tmp = new Developpement(test);
+        tmp.creationCarteDev(test);
+        tmp.afficheCarteDev();
     }
 }
-*/
+
+
