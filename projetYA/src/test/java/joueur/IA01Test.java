b17a8ad6e5315e82f3be196721a84ea7d84eb15d
Mojigot7 <ayyacine.pro@gmail.com>
2021-12-31 19:27:02 +0100

    ajout des tests

---
+++ projetYA/src/test/java/joueur/IA01Test.java
@@ -0,0 +1,85 @@
+package joueur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Color;
+
+import org.junit.jupiter.api.Test;
+
+import plateau.Plateau;
+
+class IA01Test {
+
+	@Test
+	void poserRoute() {
+		Plateau p = new Plateau();
+		Joueur j = new IA("BOT1",Color.BLUE);
+		j.poserColonie(p);
+		p.affiche();
+		j.poserRoute(p);
+		System.out.println();
+	}
+	
+	@Test
+	void poserColonie() {
+		Plateau p = new Plateau();
+		Joueur j = new IA("BOT1",Color.BLUE);
+		j.poserColonie(p);
+		j.poserColonie(p);
+		p.affiche();
+		System.out.println();
+	}
+	
+	@Test
+	void poserVille() {
+		Plateau p = new Plateau();
+		Joueur j = new IA("BOT1",Color.BLUE);
+		j.poserColonie(p);
+		j.poserVille(p);
+		p.affiche();
+		System.out.println();
+	}
+	
+	@Test
+	void deplacerVoleur() {
+		Plateau p = new Plateau();
+		Joueur j = new IA("BOT1",Color.BLUE);
+		j.deplacerVoleur(p);
+		p.affiche();
+		System.out.println();
+	}
+	
+	@Test
+	void jeterSesRessources() {
+		Joueur j = new IA("BOT1",Color.BLUE);
+		j.getInventaire().getRessource().replace("BOIS", 8);
+		j.jeterSesRessources();
+		assertEquals(4,j.getInventaire().getRessource().get("BOIS"));
+	}
+	
+	@Test
+	void donnerRessource() {
+		Joueur j = new IA("BOT1",Color.BLUE);
+		j.getInventaire().getRessource().replace("BOIS", 4);
+		j.donnerRessource();
+		assertEquals(3, j.getInventaire().getRessource().get("BOIS"));
+	}
+	
+	@Test
+	void maxColonie() {
+		Plateau p = new Plateau();
+		Joueur j = new IA("BOT1",Color.BLUE);
+		j.poserColonie(p);
+		j.poserColonie(p);
+		j.poserColonie(p);
+		j.poserColonie(p);
+		j.poserColonie(p);
+		j.poserColonie(p);
+		j.poserColonie(p);
+		j.poserColonie(p);
+		j.poserColonie(p);
+		assertEquals(true,p.coloniePleine());
+		p.affiche();
+		System.out.println();
+	}
+}
