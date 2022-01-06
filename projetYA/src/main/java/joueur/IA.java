962439f6e16ce0ff7ad4c43657c8040a08d25acd
Zhou Alexandre <zhoualexandre@gmail.com>
2022-01-06 22:15:01 +0100

    suppression d'import non utilise

--- projetYA/src/main/java/joueur/IA.java
+++ projetYA/src/main/java/joueur/IA.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Random;
-import java.util.Scanner;
 
 import plateau.Plateau;
 
--- projetYA/src/main/java/plateau/Tuile.java
+++ projetYA/src/main/java/plateau/Tuile.java
@@ -1,42 +1,22 @@
 package plateau;
 
-import java.awt.Color;
-
 import colors.ConsoleColors;
 import joueur.Joueur;
 
 public abstract class Tuile {
-	/*private int x;
-	private int y;*/
+	
 	private int numero;
 	private String type;
 	private boolean voleur;
 	private Joueur estVoleur;
 	
 	public Tuile(int num, String type, boolean voleur) {
-		/*this.x = x;
-		this.y = y;*/
 		this.numero = num;
 		this.type = type;
 		this.voleur = voleur;
 		this.estVoleur = null;
 	}
-	
-	/*public int getX() {
-		return x;
-	}
-	
-	public void setX(int x) {
-		this.x = x;
-	}
-	
-	public int getY() {
-		return y;
-	}
-	
-	public void setY(int y) {
-		this.y = y;
-	}*/
+
 	
 	public abstract boolean estCroisement();
 	public abstract boolean estSommet();
